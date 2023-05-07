d46e81f5aa7b0e1db7f9cd9ecc623f450b6b1fe9
Salma Ahmed <salmahamzaahmed77@gmail.com>
2023-05-07 05:01:44 +0300

    [useEffect] connected btn

---
+++ components/ManualHeader.jsx
@@ -0,0 +1,45 @@
+import { useMoralis } from "react-moralis"
+import { useEffect } from "react"
+export default function ManualHeader() {
+    const { enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3 } = useMoralis() //Moralis has many accounts
+    useEffect(() => {
+        if (isWeb3Enabled) return
+        if(typeof window != "undefined"){
+             if (window.localStorage.getItem("connected")) {
+            //if the key of connected is exist
+            enableWeb3()
+        }
+        }
+       
+    }, [isWeb3Enabled])
+    useEffect(() => {
+        Moralis.onAccountChanged((account) => {
+            console.log(`Account changed to ${account}`)
+            if (account == null) {
+                window.localStorage.removeItem("connected")
+                deactivateWeb3()
+                console.log("Null account found!")
+            }
+        })
+    }, [])
+    return (
+        <div>
+            {account ? (
+                <div>
+                    Connected to {account.slice(0, 6)}...{account.length - 4}
+                </div>
+            ) : (
+                <button
+                    onClick={async () => {
+                        await enableWeb3()
+                        if (typeof window != "undefined") {
+                            window.localStorage.setItem("connected", "injected")
+                        }
+                    }}
+                >
+                    Connect
+                </button>
+            )}
+        </div>
+    )
+}
