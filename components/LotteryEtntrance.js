e1a4dc0e9c1873535d990ad671078ac94af41028
Salma Ahmed <salmahamzaahmed77@gmail.com>
2023-05-14 16:22:56 +0300

    frontend connected to back(hardhat-..-lottery)

---
+++ components/LotteryEtntrance.js
@@ -0,0 +1,15 @@
+import { useWeb3Contract } from "react-moralis"
+//have a function to call a lottery (to enter the entrance)
+export default function LotteryEntrance(){
+    //Function we will call to enter the lottery (runContractFunction)
+// const {runContractFunction:enterRaffle} = useWeb3Contract({
+//     abi:,
+//     contractAddress:
+//     functionName:,
+//     params:{},
+//     msgValue:
+// })
+    return (
+        <div></div>
+    )
+}
