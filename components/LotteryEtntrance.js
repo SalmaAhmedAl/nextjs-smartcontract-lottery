aefa658c43a9746aa46f0085e9cbb154730c7f7b
Salma Ahmed <salmahamzaahmed77@gmail.com>
2023-05-17 00:57:24 +0300

    [ISSUE SOLVED] getEntranceFee undefined

--- components/LotteryEtntrance.js
+++ components/LotteryEtntrance.js
@@ -1,15 +1,45 @@
 import { useWeb3Contract } from "react-moralis"
+import { abi, contractAddresses } from "../constants"
+import Raffle from "../constants/Raffle.json"
+import { useMoralis } from "react-moralis"
+import { useEffect, useState } from "react"
+import { ethers } from "ethers"
 //have a function to call a lottery (to enter the entrance)
-export default function LotteryEntrance(){
+export default function LotteryEntrance() {
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
+    const [entranceFee, setEntranceFee] = useState("0") //entranceFee is a variable and "0" ia starting value.. setEntranceFee is a function
+
     //Function we will call to enter the lottery (runContractFunction)
-// const {runContractFunction:enterRaffle} = useWeb3Contract({
-//     abi:,
-//     contractAddress:
-//     functionName:,
-//     params:{},
-//     msgValue:
-// })
+    // const {runContractFunction:enterRaffle} = useWeb3Contract({
+    //     abi:abi,
+    //     contractAddress:raffleAddress,
+    //     functionName:"enterRaffle",
+    //     params:{},
+    //     msgValue:
+    // })
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            //try to read raffle entrace fee value
+            async function updateUi() {
+                const entranceFeeFromCall = (await getEntranceFee()).toString()
+                console.log(entranceFeeFromCall)
+                setEntranceFee(ethers.utils.formatUnits(entranceFeeFromCall, "ether"))
+            }
+            updateUi()
+        }
+    }, [isWeb3Enabled])
     return (
-        <div></div>
+        <div>
+            Hi from Lottery entrance!<div>{entranceFee}</div>
+        </div>
     )
 }
